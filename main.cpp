a4342cbbd5a6b36729b07611cea5111bf01379b5
Diego Soares <dieg657@gmail.com>
2018-04-05 03:14:06 -0300

    Feita a integração do Python com o C++, agora os calculos pesados são feitos pelo Python!

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "veiculo.h"
 #include "via.h"
 #include "utilidades.h"
-
+#include "pycalculos.h"
 
 using namespace std;
 using namespace boost::python;
@@ -10,18 +10,11 @@ using namespace boost::python;
 int main()
 {
     Py_Initialize();
-     PyObject* filename = PyString_FromString((char*)"utilidades");
-         PyObject* imp = PyImport_Import(filename);
-         PyObject* func = PyObject_GetAttrString(imp,(char*)"someFunction");
-         PyObject* args = PyTuple_Pack(1,PyString_FromString("CacheSetup"));
-         PyObject* retured_value = PyObject_CallObject(func, args); // if you have arg
-         double result = PyFloat_AsDouble(retured_value);
-     std::cout << result << std::endl;
-     Py_Finalize();
+    PyC * a = new PyC();
 
 
-
-
+    cout << setprecision(1) << fixed << "Bloqueio: " << a->calcularBloqueio(10,11) << "%" << endl;
+    cout << setprecision(1) << fixed << "Probabilidade de espera: " << a->calcularProbEspera(10,11) << "%" << endl;
     return 0;
 
 }
